daf20dcc573a90c2ef107e9fffaccc189075be7c
Siroyan <1517053@ed.tus.ac.jp>
2020-09-07 12:36:28 +0000

    [add] TodoForm comp

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,25 @@
 <template>
   <div id="app" class="container text-center">
-    <div>
-      <form class="row" @submit.prevent="addTodo">
-        <!-- やることタイトル -->
-        <div class="form-group col-10">
-          <input type="text" class="form-control" id="todo-title" placeholder="やること" v-model="newTodo.title">
-        </div>
-        <!-- 登録ボタン -->
-        <div class="form-group col-2">
-          <button type="submit" class="btn btn-primary">登録</button>
-        </div>
-      </form>
-    </div>
+    <TodoForm @add-todo="addTodo"></TodoForm>
     <Todolist :todos="todos" @delete-todo="deleteTodo"></Todolist>
   </div>
 </template>
 
 <script>
 import Todolist from './components/Todolist.vue'
+import TodoForm from './components/TodoForm.vue'
 export default {
   name: 'app',
   components: {
-    Todolist
+    Todolist,
+    TodoForm
   },
   data: () => ({
-    newTodo: {
-      id: '',
-      title: '',
-      isDone: false
-    },
     todos: []
   }),
   methods: {
-    addTodo: function() {
-      if (this.newTodo.title === '') return;
-      let item = {
-        id: new Date().getTime().toString(16),
-        title: this.newTodo.title,
-        isDone: this.newTodo.isDone
-      };
-      // リストに追加
-      this.todos.push(item);
-      // 空に戻しておく
-      this.newTodo = {
-        id: '',
-        title: '',
-        isDone: false
-      };
+    addTodo: function(newTodo) {
+      this.todos.push(newTodo);
     },
     deleteTodo: function(id) {
       this.todos = this.todos.filter(function(todo) {
@@ -56,7 +29,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-
-</style>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <form class="row" @submit.prevent="addTodo">
+    <!-- やることタイトル -->
+    <div class="form-group col-10">
+      <input type="text" class="form-control" id="todo-title" placeholder="やること" v-model="newTodo.title">
+    </div>
+    <!-- 登録ボタン -->
+    <div class="form-group col-2">
+      <button type="submit" class="btn btn-primary">登録</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'TodoForm',
+  data: () => ({
+    newTodo: {
+      id: '',
+      title: '',
+      isDone: false
+    }
+  }),
+  methods: {
+    addTodo: function() {
+      if (this.newTodo.title === '') return;
+      this.newTodo.id = new Date().getTime().toString(16);
+      this.$emit('add-todo', this.newTodo);
+      // 空に戻しておく
+      this.newTodo = {
+        id: '',
+        title: '',
+        isDone: false
+      };
+    }
+  }
+}
+</script>
